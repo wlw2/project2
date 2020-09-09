eeaa040c153aba90d4882b44520dcead0242937f
wlw2 <69854188+wlw2@users.noreply.github.com>
2020-09-09 23:38:06 +0800

    Add files via upload

---
+++ pb/src/main/java/pb/protocols/keepalive/KeepAliveProtocol.java
@@ -0,0 +1,197 @@
+package pb.protocols.keepalive;
+import java.util.logging.Logger;
+
+import pb.Endpoint;
+import pb.EndpointUnavailable;
+import pb.Manager;
+import pb.Utils;
+import pb.protocols.Message;
+import pb.protocols.Protocol;
+import pb.protocols.IRequestReplyProtocol;
+import pb.protocols.session.SessionStartReply;
+import pb.protocols.session.SessionStartRequest;
+import pb.protocols.session.SessionStopReply;
+import pb.server.ServerManager;
+
+/**
+ * Provides all of the protocol logic for both client and server to undertake
+ * the KeepAlive protocol. In the KeepAlive protocol, the client sends a
+ * KeepAlive request to the server every 20 seconds using
+ * {@link pb.Utils#setTimeout(pb.protocols.ICallback, long)}. The server must
+ * send a KeepAlive response to the client upon receiving the request. If the
+ * client does not receive the response within 20 seconds (i.e. at the next time
+ * it is to send the next KeepAlive request) it will assume the server is dead
+ * and signal its manager using
+ * {@link pb.Manager#endpointTimedOut(Endpoint,Protocol)}. If the server does
+ * not receive a KeepAlive request at least every 20 seconds (again using
+ * {@link pb.Utils#setTimeout(pb.protocols.ICallback, long)}), it will assume
+ * the client is dead and signal its manager. Upon initialisation, the client
+ * should send the KeepAlive request immediately, whereas the server will wait
+ * up to 20 seconds before it assumes the client is dead. The protocol stops
+ * when a timeout occurs.
+ *
+ * @see {@link pb.Manager}
+ * @see {@link pb.Endpoint}
+ * @see {@link pb.protocols.Message}
+ * @see {@link pb.protocols.keepalive.KeepAliveRequest}
+ * @see {@link pb.protocols.keepalive.KeepAliveReply}
+ * @see {@link pb.protocols.Protocol}
+ * @author aaron
+ *
+ */
+public class KeepAliveProtocol extends Protocol implements IRequestReplyProtocol {
+	private static Logger log = Logger.getLogger(KeepAliveProtocol.class.getName());
+	private Utils utils;
+	/**
+	 * Name of this protocol.
+	 */
+	public static final String protocolName = "KeepAliveProtocol";
+	public Boolean ReceiveReply = false;
+	public Boolean ReceiveRequest = false;
+	public Boolean first = true;
+
+	/**
+	 * Initialise the protocol with an endopint and a manager.
+	 *
+	 * @param endpoint
+	 * @param manager
+	 */
+	public KeepAliveProtocol(Endpoint endpoint, Manager manager) {
+		super(endpoint, manager);
+		utils = new Utils();
+	}
+
+	/**
+	 * @return the name of the protocol
+	 */
+	@Override
+	public String getProtocolName() {
+		return protocolName;
+	}
+
+	/**
+	 *
+	 */
+	@Override
+	public void stopProtocol() {
+		ReceiveReply = false;
+		ReceiveRequest = false;
+		first = true;
+	}
+
+	/*
+	 * Interface methods
+	 */
+
+	/**
+	 *
+	 */
+	public void startAsServer()  {
+		ReceiveRequest = true;
+		try {
+			sendReply(new KeepAliveRequest());
+		} catch (EndpointUnavailable endpointUnavailable) {
+			endpointUnavailable.printStackTrace();
+		}
+	}
+
+	/**
+	 *
+	 */
+	public void checkClientTimeout() {
+		Utils.getInstance().setTimeout(()->{
+			if(ReceiveRequest){
+				try {
+					sendReply(new KeepAliveRequest());
+				} catch (EndpointUnavailable endpointUnavailable) {
+					endpointUnavailable.printStackTrace();
+				}
+				return;
+			}else {
+				manager.endpointTimedOut(endpoint, this);
+			}
+		},20050);
+
+	}
+
+	/**
+	 *
+	 */
+	public void startAsClient() throws EndpointUnavailable {
+		ReceiveReply = true;
+		sendRequest(new KeepAliveRequest());
+	}
+
+	/**
+	 * @param msg
+	 */
+	@Override
+	public void sendRequest(Message msg)  {
+		if(first) {
+			try {
+				endpoint.send(msg);
+			} catch (EndpointUnavailable endpointUnavailable) {
+				endpointUnavailable.printStackTrace();
+			}
+
+			ReceiveReply = false;
+			first = false;
+		}
+
+		else {
+
+			Utils.getInstance().setTimeout(() -> {
+				if (ReceiveReply) {
+					try {
+						sendRequest(new KeepAliveRequest());
+						endpoint.send(msg);
+					} catch (EndpointUnavailable endpointUnavailable) {
+						endpointUnavailable.printStackTrace();
+					}
+					ReceiveReply = false;
+					return;
+				} else {
+					manager.endpointTimedOut(endpoint, this);
+				}
+			}, 20000);
+		}
+
+	}
+
+
+	/**
+	 * @param msg
+	 */
+	@Override
+	public void receiveReply(Message msg) throws EndpointUnavailable {
+		if (msg instanceof KeepAliveReply) {
+			ReceiveReply = true;
+			sendRequest(new KeepAliveRequest());
+		}
+	}
+
+	/**
+	 * @param msg
+	 * @throws EndpointUnavailable
+	 */
+	@Override
+	public void receiveRequest(Message msg) throws EndpointUnavailable {
+		if (msg instanceof KeepAliveRequest) {
+			ReceiveRequest = true;
+			sendReply(new KeepAliveReply());
+		}
+	}
+
+	/**
+	 * @param msg
+	 */
+	@Override
+	public void sendReply(Message msg) throws EndpointUnavailable {
+		endpoint.send(msg);
+		Utils.getInstance().setTimeout(() -> {
+			ReceiveRequest = false;
+		}, 19000);
+		checkClientTimeout();
+	}
+}
+
---
+++ pb/src/main/java/pb/protocols/session/SessionProtocol.java
@@ -0,0 +1,213 @@
+package pb.protocols.session;
+
+
+import java.util.logging.Logger;
+
+import pb.Endpoint;
+import pb.EndpointUnavailable;
+import pb.Manager;
+import pb.Utils;
+import pb.protocols.Message;
+import pb.protocols.Protocol;
+import pb.protocols.IRequestReplyProtocol;
+
+/**
+ * Allows the client to request the session to start and to request the session
+ * to stop, which in turns allows the sockets to be properly closed at both
+ * ends. Actually, either party can make such requests, but usually the client
+ * would make the session start request as soon as it connects, and usually the
+ * client would make the session stop request. The server may however send a
+ * session stop request to the client if it wants (needs) to stop the session,
+ * e.g. perhaps the server is becoming overloaded and needs to shed some
+ * clients.
+ *
+ * @see {@link pb.Manager}
+ * @see {@link pb.Endpoint}
+ * @see {@link pb.protocols.Protocol}
+ * @see {@link pb.protocols.IRequestReplyProtocol}
+ * @see {@link pb.protocols.session.SessionStartRequest}
+ * @see {@link pb.protocols.session.SessionStartReply}
+ * @see {@link pb.protocols.session.SessionStopRequest}
+ * @see {@link pb.protocols.session.SessionStopReply}
+ * @author aaron
+ *
+ */
+public class SessionProtocol extends Protocol implements IRequestReplyProtocol {
+	private static Logger log = Logger.getLogger(SessionProtocol.class.getName());
+	private Message msg;
+	/**
+	 * The unique name of the protocol.
+	 */
+	public static final String protocolName="SessionProtocol";
+
+	// Use of volatile is in case the thread that calls stopProtocol is different
+	// to the endpoint thread, although in this case it hardly needed.
+
+	/**
+	 * Whether the protocol has started, i.e. start request and reply have been sent,
+	 * or not.
+	 */
+	private volatile boolean protocolRunning=false;
+	private volatile boolean receiveStartRequest = false;
+	private volatile boolean receiveStartReply = false;
+	/**
+	 * Initialise the protocol with an endpoint and manager.
+	 * @param endpoint
+	 * @param manager
+	 */
+	public SessionProtocol(Endpoint endpoint, Manager manager) {
+		super(endpoint,manager);
+	}
+
+	/**
+	 * @return the name of the protocol.
+	 */
+	@Override
+	public String getProtocolName() {
+		return protocolName;
+	}
+	/**
+	 * If this protocol is stopped while it is still in the running
+	 * state then this indicates something may be a problem.
+	 */
+	@Override
+	public void stopProtocol() {
+		if(protocolRunning) {
+			log.severe("protocol stopped while it is still underway");
+		}
+	}
+
+	/*
+	 * Interface methods
+	 */
+
+
+	/**
+	 * Called by the manager that is acting as a client.
+	 */
+	@Override
+	public void startAsClient() throws EndpointUnavailable {
+		//  send the server a start session request
+		sendRequest(new SessionStartRequest());
+		Utils.getInstance().setTimeout(() -> {
+			this.receiveReply(msg);
+			if(msg == null){
+				manager.endpointTimedOut(endpoint, this);
+			}
+		}, 20000);
+	}
+	/**
+	 * Called by the manager that is acting as a server.
+	 */
+	@Override
+	public void startAsServer() {
+		Utils.getInstance().setTimeout(() -> {
+			try {
+				this.receiveRequest(msg);
+			} catch (EndpointUnavailable endpointUnavailable) {
+				endpointUnavailable.printStackTrace();
+			}
+			if(msg == null){
+				manager.endpointTimedOut(endpoint, this);
+			}
+		}, 20000);
+	}
+	/**
+	 * Generic stop session call, for either client or server.
+	 * @throws EndpointUnavailable if the endpoint is not ready or has terminated
+	 */
+	public void stopSession() throws EndpointUnavailable {
+		sendRequest(new SessionStopRequest());
+	}
+
+	/**
+	 * Just send a request, nothing special.
+	 * @param msg
+	 */
+	@Override
+	public void sendRequest(Message msg) throws EndpointUnavailable {
+		Utils.getInstance().setTimeout(() -> {
+			try {
+		endpoint.send(msg);
+			} catch (EndpointUnavailable endpointUnavailable) {
+				endpointUnavailable.printStackTrace();
+			}
+		}, 20000);
+	}
+
+
+	/**
+	 * If the reply is a session start reply then tell the manager that
+	 * the session has started, otherwise if its a session stop reply then
+	 * tell the manager that the session has stopped. If something weird
+	 * happens then tell the manager that something weird has happened.
+	 * @param msg
+	 */
+	@Override
+	public void receiveReply(Message msg) {
+		if (msg instanceof SessionStartReply) {
+			if (protocolRunning) {
+				// error, received a second reply?
+				manager.protocolViolation(endpoint, this);
+				return;
+			}
+			protocolRunning = true;
+			manager.sessionStarted(endpoint);
+		} else if (msg instanceof SessionStopReply) {
+			if (!protocolRunning) {
+				// error, received a second reply?
+				manager.protocolViolation(endpoint, this);
+				return;
+			}
+			protocolRunning = false;
+			manager.sessionStopped(endpoint);
+		}
+
+	}
+
+	/**
+	 * If the received request is a session start request then reply and
+	 * tell the manager that the session has started. If the received request
+	 * is a session stop request then reply and tell the manager that
+	 * the session has stopped. If something weird has happened then...
+	 * @param msg
+	 */
+	@Override
+	public void receiveRequest(Message msg) throws EndpointUnavailable {
+		System.out.print("SERVER SIDE: " + "\n");
+		if(msg instanceof SessionStartRequest) {
+			if(protocolRunning) {
+				// error, received a second request?
+				manager.protocolViolation(endpoint,this);
+				return;
+			}
+			protocolRunning=true;
+			sendReply(new SessionStartReply());
+			manager.sessionStarted(endpoint);
+		} else if(msg instanceof SessionStopRequest) {
+			if(!protocolRunning) {
+				// error, received a second request?
+				manager.protocolViolation(endpoint,this);
+			}
+			protocolRunning=false;
+			sendReply(new SessionStopReply());
+			manager.sessionStopped(endpoint);
+		}
+	}
+
+	/**
+	 * Just send a reply, nothing special to do.
+	 * @param msg
+	 */
+
+	@Override
+	public void sendReply(Message msg) throws EndpointUnavailable {
+		Utils.getInstance().setTimeout(() -> {
+			try {
+		endpoint.send(msg);
+			} catch (EndpointUnavailable endpointUnavailable) {
+				endpointUnavailable.printStackTrace();
+			}
+		}, 20000);
+	}
+}
